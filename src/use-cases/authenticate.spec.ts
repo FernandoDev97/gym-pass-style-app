7cfdc43260ecfe8e01234c10ff48c0648fce01c3
FernandoDev97 <l.fernando.dev@gmail.com>
2024-01-17 22:32:00 -0300

    add check ins use case and gaet user profile use case with tets

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -29,7 +29,7 @@ describe('Autheticate Use Case', () => {
   })
 
   it('should be able to authenticate with wrong email', async () => {
-    expect(() =>
+    await expect(() =>
       sut.handle({
         email: 'johndoe@example.com',
         password: '123456',
@@ -44,7 +44,7 @@ describe('Autheticate Use Case', () => {
       password_hash: await hash('123456', 6),
     })
 
-    expect(() =>
+    await expect(() =>
       sut.handle({
         email: 'johndoe@example.com',
         password: '123123',
---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,44 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let userRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    userRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(userRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await userRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.handle({
+      userId: createdUser.id,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual('John Doe')
+  })
+
+  it('should be able to get profile with wrong id', async () => {
+    await userRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    await expect(() =>
+      sut.handle({
+        userId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
